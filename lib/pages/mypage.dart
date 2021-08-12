d5be7a7784a3199157818bee1dc2b3a1ab21cafa
bonghee <hae8064@nate.com>
2021-08-12 18:58:20 +0900

    점심/저녁 기능 추가 (Home 부분)

--- lib/pages/mypage.dart
+++ lib/pages/mypage.dart
@@ -1,30 +1,299 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:firebase/pages/sign_in.dart';
+import 'package:firebase/random_choice/dinner.dart';
+import 'package:firebase/random_choice/lunch.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
-class MyPage extends StatelessWidget {
+//MyPage_Home 부분
+
+//carousel slider 부분 이미지 (음식사진)
+final List<String> imageList = [
+  'https://lh3.googleusercontent.com/proxy/ehTgE2_xhY8L8zLKOKvh1O3av5fGXBvWz0ZqED3j_t5T9gp3bq_9HfvQcMfJxhGcTyZnlrPsi0pmDCkq5DAhckNyffnDtOr81e2f8A'
+      'https://www.newiki.net/w/images/thumb/d/d9/Jjajangmyeon.jpg/450px-Jjajangmyeon.jpg'
+];
+
+class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
 
   @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  int _selectedIndex = 1; //현재 선택된 페이지
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MY Page"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("환영합니다.\n${FirebaseAuth.instance.currentUser!.email}"),
-                ElevatedButton(
-                    onPressed: FirebaseAuth.instance.signOut,
-                    child: Text("로그아웃")),
-              ],
-            ),
-          ))
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green[300],
+          title: SizedBox(width: 300,
+              child: Text("오늘 뭐먹지?")),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('imgs/profile.jpg'),
+                ),
+                accountName: Text("맛집 탐방러"),
+                accountEmail: Text("${FirebaseAuth.instance.currentUser!.email}"),
+                onDetailsPressed: (){
+                  print("arrow is clicked");
+                },
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0)
+                  )
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.grey[850],
+                ),
+                title: Text('Home'),
+                onTap: () {
+                  print('Home is clicked');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.grey[850],
+                ),
+                title: Text('Setting'),
+                onTap: () {
+                  print('Setting is clicked');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.grey[850],
+                ),
+                title: Text('Logout'),
+                onTap: () {
+                  FirebaseAuth.instance.signOut;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));
+                },
+                trailing: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+
+
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.grey,
+          selectedItemColor: Colors.lightGreenAccent,
+          unselectedItemColor: Colors.white.withOpacity(.60),
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          currentIndex: _selectedIndex,
+          //현재 선택된 Index
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+
+              if (_selectedIndex == 2) {
+                //맛집 랭킹
+              } else if (_selectedIndex == 0) {
+                //지역별
+              }
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.map_outlined), title: Text("지역별")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.star), title: Text("맛집랭킹")),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Stack(
+                fit: StackFit.passthrough,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0x665ac18e),
+                            Color(0x995ac18e),
+                            Color(0xcc5ac18e),
+                            Color(0xff5ac18e),
+                          ]),
+                    ),
+                  ),
+                ],
+              ),
+
+              //상단 배너 부분 이미지
+              ImageSlideshow(
+                height: 200,
+                initialPage: 0,
+                indicatorColor: Colors.blue,
+                indicatorBackgroundColor: Colors.green,
+                children: [
+                  Image.network(
+                      'https://lh3.googleusercontent.com/proxy/ehTgE2_xhY8L8zLKOKvh1O3av5fGXBvWz0ZqED3j_t5T9gp3bq_9HfvQcMfJxhGcTyZnlrPsi0pmDCkq5DAhckNyffnDtOr81e2f8A',
+                      fit: BoxFit.cover),
+                  Image.network(
+                    'https://www.newiki.net/w/images/thumb/d/d9/Jjajangmyeon.jpg/450px-Jjajangmyeon.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://lh3.googleusercontent.com/proxy/dzTfp6d9-cJJum9I0GWFrngk1huaXHXrAz-g_K9y5sFSHLSQ8e3nmpDQeBX-38bOz7D-ozSKNCC_AV-q8KRw3hTaWh81tCQ2jNHocFk7fEf5-nJFGbnjYmNIlGAnXe0BataSSlfPulwn6ro82BMt9NnVaRk67e8Zd0EmPuZ1y3aeP4tDATXe1A',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://ai.esmplus.com/foodjang01/images/221500368_b_1.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                      'https://lh3.googleusercontent.com/proxy/ehTgE2_xhY8L8zLKOKvh1O3av5fGXBvWz0ZqED3j_t5T9gp3bq_9HfvQcMfJxhGcTyZnlrPsi0pmDCkq5DAhckNyffnDtOr81e2f8A',
+                      fit: BoxFit.cover),
+                  Image.network(
+                    'https://www.newiki.net/w/images/thumb/d/d9/Jjajangmyeon.jpg/450px-Jjajangmyeon.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://lh3.googleusercontent.com/proxy/dzTfp6d9-cJJum9I0GWFrngk1huaXHXrAz-g_K9y5sFSHLSQ8e3nmpDQeBX-38bOz7D-ozSKNCC_AV-q8KRw3hTaWh81tCQ2jNHocFk7fEf5-nJFGbnjYmNIlGAnXe0BataSSlfPulwn6ro82BMt9NnVaRk67e8Zd0EmPuZ1y3aeP4tDATXe1A',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://ai.esmplus.com/foodjang01/images/221500368_b_1.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                      'https://lh3.googleusercontent.com/proxy/ehTgE2_xhY8L8zLKOKvh1O3av5fGXBvWz0ZqED3j_t5T9gp3bq_9HfvQcMfJxhGcTyZnlrPsi0pmDCkq5DAhckNyffnDtOr81e2f8A',
+                      fit: BoxFit.cover),
+                  Image.network(
+                    'https://www.newiki.net/w/images/thumb/d/d9/Jjajangmyeon.jpg/450px-Jjajangmyeon.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://lh3.googleusercontent.com/proxy/dzTfp6d9-cJJum9I0GWFrngk1huaXHXrAz-g_K9y5sFSHLSQ8e3nmpDQeBX-38bOz7D-ozSKNCC_AV-q8KRw3hTaWh81tCQ2jNHocFk7fEf5-nJFGbnjYmNIlGAnXe0BataSSlfPulwn6ro82BMt9NnVaRk67e8Zd0EmPuZ1y3aeP4tDATXe1A',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://ai.esmplus.com/foodjang01/images/221500368_b_1.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                      'https://lh3.googleusercontent.com/proxy/ehTgE2_xhY8L8zLKOKvh1O3av5fGXBvWz0ZqED3j_t5T9gp3bq_9HfvQcMfJxhGcTyZnlrPsi0pmDCkq5DAhckNyffnDtOr81e2f8A',
+                      fit: BoxFit.cover),
+                  Image.network(
+                    'https://www.newiki.net/w/images/thumb/d/d9/Jjajangmyeon.jpg/450px-Jjajangmyeon.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://lh3.googleusercontent.com/proxy/dzTfp6d9-cJJum9I0GWFrngk1huaXHXrAz-g_K9y5sFSHLSQ8e3nmpDQeBX-38bOz7D-ozSKNCC_AV-q8KRw3hTaWh81tCQ2jNHocFk7fEf5-nJFGbnjYmNIlGAnXe0BataSSlfPulwn6ro82BMt9NnVaRk67e8Zd0EmPuZ1y3aeP4tDATXe1A',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    'https://ai.esmplus.com/foodjang01/images/221500368_b_1.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ],
+                onPageChanged: (value) {
+                  print("Page changed :  $value");
+                },
+                autoPlayInterval: 2000,
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+              //점심 부분
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Lunch()));
+                    },
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 80.0,
+                          backgroundImage: AssetImage('imgs/lunch.PNG'),
+                        ),
+                        Text(
+                          "점심",
+                          style: TextStyle(
+                              fontSize: 25, color: Colors.orange[300]),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 20,
+                  ),
+
+                  //저녁 부분
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Dinner()),
+                      );
+                    },
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 80.0,
+                          backgroundImage: AssetImage('imgs/dinner.PNG'),
+                        ),
+                        Text(
+                          "저녁",
+                          style: TextStyle(fontSize: 25, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,10 +1,12 @@
+import 'package:firebase/pages/mypage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+//처음 메인화면
+//로그인 조건(id는 비어있으면 안되고, 비밀번호는 6글자 이상 써야한다.)
 //회원가입 페이지
-//자동 로그인 되는거다.
 
 class SignIn extends StatefulWidget {
   @override
@@ -12,6 +14,7 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  late final Function toggleScreen;
   final _init = Firebase.initializeApp();
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
@@ -23,12 +26,64 @@ class _SignInState extends State<SignIn> {
     super.initState();
   }
 
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  _logIn() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text, password: _passwordController.text);
+
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => MyPage()));
+    } on FirebaseAuthException catch (e) {
+      var message = "";
+
+      switch (e.code) {
+        case 'invalid-email':
+          message = "입력한 이메일이 잘못되었습니다.";
+          break;
+
+        case 'user-disabled':
+          message = "로그인을 시도한 사용자는 비활성화 되어 있습니다.";
+          break;
+
+        case 'user-not':
+          message = "로그인을 시도한 사용자를 찾을 수 없습니다.";
+          break;
+
+        case 'wrong-password':
+          message = "비밀번호가 틀립니다.";
+          break;
+      }
+
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("로그인을 실패했습니다."),
+              content: Text(message),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("확인"),
+                )
+              ],
+            );
+          });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.green
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
         body: AnnotatedRegion<SystemUiOverlayStyle>(
           value: SystemUiOverlayStyle.light,
@@ -45,97 +100,101 @@ class _SignInState extends State<SignIn> {
                             begin: Alignment.topCenter,
                             end: Alignment.bottomCenter,
                             colors: [
-                              Color(0x665ac18e),
-                              Color(0x995ac18e),
-                              Color(0xcc5ac18e),
-                              Color(0xff5ac18e),
-                            ])),
+                          Color(0x665ac18e),
+                          Color(0x995ac18e),
+                          Color(0xcc5ac18e),
+                          Color(0xff5ac18e),
+                        ])),
                     child: SingleChildScrollView(
                       physics: AlwaysScrollableScrollPhysics(),
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 25, vertical: 80),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 25, vertical: 80),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                        Text(
-                        "오늘 뭐먹지?",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Image(
-                        //이미지 바탕화면에 맞춰 연하게 설정
-                        image: AssetImage("imgs/question.jpg"),
-                        fit: BoxFit.cover,
-                        color: Color(0x995ac18e),
-                        colorBlendMode: BlendMode.darken,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        controller: _emailController,
-                        validator: (val) =>
-                        val!.isNotEmpty
-                            ? null
-                            : "please enter a email address.",
-                        decoration: InputDecoration(
-                            hintText: "Email",
-                            prefixIcon: Icon(Icons.mail),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            )
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        controller: _passwordController,
-                        validator: (val) =>
-                        val!.length < 6
-                            ? 'Enter more than 6 char'
-                            : null,
-                        decoration: InputDecoration(
-                            hintText: "Password",
-                            prefixIcon: Icon(Icons.vpn_key),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            )
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      MaterialButton(
-                        minWidth: double.infinity,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        color: Colors.lightGreen[400],
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            print("Email: ${_emailController.text}");
-                            print("Email: ${_passwordController.text}");
-                          }
-                        },
-                        // onPressed: _signIn,
-                        child: Text("로그인"),
-                      ),
-                      Row(
-                        children: [
-                          Text("계정이 없으신가요 ?"),
-                          SizedBox(width: 10,),
-                          TextButton(onPressed: () {},
-                              child: Text("회원가입", style: TextStyle(
-                                color: Colors.red[400]
-                              ),),
-                          )
-                              ],
+                          Text(
+                            "오늘 뭐먹지?",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Image(
+                            //이미지 바탕화면에 맞춰 연하게 설정
+                            image: AssetImage("imgs/question.jpg"),
+                            fit: BoxFit.cover,
+                            color: Color(0x995ac18e),
+                            colorBlendMode: BlendMode.darken,
+                          ),
+                          SizedBox(
+                            height: 80,
+                          ),
+                          TextFormField(
+                            controller: _emailController,
+                            validator: (val) => val!.isNotEmpty
+                                ? null
+                                : "이메일 계정을 입력해주세요",
+                            decoration: InputDecoration(
+                                hintText: "Email",
+                                prefixIcon: Icon(Icons.mail),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            validator: (val) => val!.length < 6
+                                ? "비밀번호 6자리 이상"
+                                : null,
+                            decoration: InputDecoration(
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.vpn_key),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          MaterialButton(
+                            minWidth: double.infinity,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            color: Colors.lightGreen[400],
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                print("Email: ${_emailController.text}");
+                                print("Password: ${_passwordController.text}");
+                              }
+                              _logIn();
+                              //null체크 에러나옴
+                              // _formKey.currentState!.validate() ? Navigator.push(context, MaterialPageRoute(builder: (context) => MyPage())) : null;
+                            },
+                            // onPressed: _signIn,
+                            child: Text("로그인"),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("계정이 없으신가요 ?"),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "회원가입",
+                                  style: TextStyle(color: Colors.red[400]),
+                                ),
+                              )
+                            ],
                           )
                           // Center(
                           //   child: ElevatedButton(
